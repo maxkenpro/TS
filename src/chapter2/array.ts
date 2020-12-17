2fc3096de8618ea91fabab711017afdd09f2ddf2
maxkenkomoda <tango12speedcell@gmail.com>
2020-12-17 14:12:20 +0900

    learned data types of lists

---
+++ src/chapter2/array.ts
@@ -0,0 +1,10 @@
+const fruits: string[] = ['apple', 'banana', 'Grape']
+fruits.push('melon');
+
+console.log(fruits);
+
+const fruit = fruits[0];
+console.log(fruit);
+
+const book: [string, number, boolean] = ['business', 1500, false];
+
