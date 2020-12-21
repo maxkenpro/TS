62432caa425510e2b39fa69b38ae85ca2a3fdd37
maxkenkomoda <tango12speedcell@gmail.com>
2020-12-21 16:53:36 +0900

    function type section 2

--- src/chapter2/type_function.ts
+++ src/chapter2/type_function.ts
@@ -1,3 +1,17 @@
-function add(num1, num2) {
+function add(num1: number, num2: number): number {
   return num1 + num2
 }
+
+function sayHello() {
+  console.log("Hello");
+}
+
+const anotherAdd: (n1: number, n2: number) => number = add;
+const anotherAdd2: (n1: number, n2: number) => number = function (num1: number, num2: number): number {
+  return num1 + num2
+}
+
+//arrow function
+
+const doubleNumber = (number: number) => number * 2;
+const doubleNumber2: (num: number) => number = num => num * 2;
